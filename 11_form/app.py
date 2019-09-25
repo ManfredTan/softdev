95512e9a31f00d9a06d18bd54b58fbd00ffd3e97
Manfred <mtan00@stuy.edu>
2019-09-25 14:53:45 -0400

    form

--- 11_form/app.py
+++ 11_form/app.py
@@ -12,8 +12,16 @@ def hello_world():
 @app.route("/form")
 def makeForm():
     print(app)
+    print(request.args)
     return render_template('foo.html',
     )
+
+@app.route("/auth")
+def auth():
+    print(app)
+    print(request)
+    print(request.args)
+    return "helo"
 
 if __name__ == "__main__":
     app.debug = True # Automatically updates project with save file
