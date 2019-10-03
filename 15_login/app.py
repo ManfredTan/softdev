b87d5f593a89afff67f3dc84a66af6117b8b14d0
Manfred <mtan00@stuy.edu>
2019-10-03 14:54:38 -0400

    almost done with all aspect. working on beginning redirect

--- 15_login/app.py
+++ 15_login/app.py
@@ -3,22 +3,26 @@
 #K15 - login
 #2019-10-02
 
-from flask import Flask, render_template, request, redirect, url_for
-
+from flask import Flask, render_template, request, session, redirect, url_for
+import os
 app = Flask(__name__)
 
-
+# username: hello
+# password: world
+app.secret_key = os.urandom(32)
 @app.route("/")
 def checkcookies():
     print(request.cookies.get('username'))
-    if(request.cookies.get('username') == None):
-        print('no cookies found: going to login page')
-        return render_template(
-            'login.html'
-        )
+    if(session['user'] == "hello" && session['pass'] == "world"):
+        #print('no cookies found: going to login page')
+        return redirect(url_for("welcome"))
     else:
         print('Cookies Found')
-    return url_for("welcome")
+        return render_template(
+            'login.html',
+            message = "Hello user! Please enter your username and password:"
+        )
+
 
 
 @app.route("/login", methods=["POST"])
@@ -27,20 +31,33 @@ def login():
     #print(request.args["username"])
     #print(request.args["password"])
 
-    print(request.form) #returns values in forms with POST method
-    print(request.form["username"]) #prints value in username
-    print(request.form["password"]) #prints value in password
 
-    if True:
-        return redirect(url_for("welcome")) # redirects to welcome page
+    #print(request.form) #returns values in forms with POST method
+    #print(request.form["username"]) #prints value in username
+    #print(request.form["password"]) #prints value in password
+    session['user'] = request.form["username"]
+    session['pass'] = request.form["password"]
+    #print(request.cookies.get('user'))
+    if (session['user'] == "hello"):
+        if (session['pass'] == "world"):
+            return redirect(url_for("welcome")) # redirects to welcome page
+        else:
+            return render_template(
+                'login.html',
+                message = "error: incorrect password")
     else:
-        return "error"
+        return render_template(
+            'login.html',
+            message = "error: incorrect username")
 
-    return "received login information"
+    #return "received login information"
+
 
 @app.route("/welcome")
 def welcome():
-    return "welcome you have logged in. moonlight ah."
+    return render_template(
+        "welcome.html"
+    )
 
 if __name__ == "__main__":
     app.debug = True # Automatically updates project with save file
