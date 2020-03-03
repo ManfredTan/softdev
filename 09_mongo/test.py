cd2095dfe5052fcc765a2dd8115a3927b9ce4b4e
Manfred <mtan00@stuy.edu>
2020-03-02 20:51:58 -0500

    added common locations database. user can now find meteorites near specified locations

--- 09_mongo/test.py
+++ 09_mongo/test.py
@@ -3,7 +3,7 @@ import pymongo
 from pymongo import MongoClient
 from pprint import pprint # pprint library is used to make the output look more pretty
 from bson.json_util import loads
-
+import bson
 
 client = MongoClient("mongodb://admin:password@167.172.25.9/restaurants") # defaults to port 27017
 db = client['restaurants']
@@ -15,24 +15,17 @@ for document in cursor:
 print(db.test.count())
 '''
 
-def convertDates(fileName):
-    with open(fileName, 'w') as json_file:
-        data = json.load(json_file)
-        #for store in data:
-        store = data[0]
-        grades = store['grades']
-        #print(grades)
-        for review in grades:
-            date = review['date']
-            stringDate = date['$date']
-            date.gdate
+def convertDates():
+    f = open('primer-dataset.json','r')
+    filedata = f.read()
+    f.close()
 
-            date['$date'] = stringDate
-                #print(stringDate)
-                #print("=====")
-        #for document in data:
-            #db.list.insert(document)
-convertDates('primer-dataset.json')
+    newdata = filedata.replace("$date","gdate")
+
+    f = open('primer-dataset2.json','w')
+    f.write(newdata)
+    f.close()
+#convertDates()
 
 def addData(fileName):
     with open(fileName) as json_file:
@@ -40,27 +33,15 @@ def addData(fileName):
         store = data[0]
         grades = store['grades']
         #print(grades)
-        for review in grades:
-            date = review['date']
-            jsonDate = date['$date']
-            print(jsonDate)
-            print("=====")
-        #for document in data:
-            #db.list.insert(document)
-#addData('primer-dataset.json')
+        for document in data:
+            db.list.insert(document)
+addData('primer-dataset2.json')
 
 
 def testAdd():
     db.test.insert({'test3':'from py'})
 
-#def addData():
-#    with open('primer-dataset.json') as f:
-#        file_data = json.load(f)
-#    collection_currency.insert_many(file_data)
-#    return null
-def ingest(f):
-    with open('primer-dataset.json') as _f:
-        return loads(f'[{",".join(map(lambda s: s[:-1], _f))}]')
+
 
 #ingest()
 #addData()
---
+++ 10_mongo/README.md
@@ -0,0 +1,12 @@
+Nasa:
+https://data.nasa.gov/Space-Science/Meteorite-Landings/gh4g-9sfh
+
+Mapquest:
+https://developer.mapquest.com/plan_purchase/steps/business_edition/business_edition_free/register
+
+Dataset json:
+https://data.nasa.gov/resource/y77d-th95.json
+
+## How to use
+1. Run initialize ```python3 initialize.py```
+2. Run main ```python3 main.py```
